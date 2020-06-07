0315ac4985b5b22d57b9580987f591fe34118345
xochozomatli <aavillaverde11@gmail.com>
2020-06-07 04:40:47 -0600

    unbreaks it, kind of

--- api/app/models.py
+++ api/app/models.py
@@ -58,13 +58,13 @@ class User(PaginatedAPIMixin, db.Model):
     token = db.Column(db.String(32), index=True, unique=True)
     token_expiration = db.Column(db.DateTime)
     ###Add a user lesson db
-    tlahtolli = db.relationship('Tlahtolli', backref='user_id', lazy='dynamic')
-    authored = db.relationship('Lesson', backref='author_id', lazy='dynamic')
+    tlahtolli = db.relationship('Tlahtolli', foreign_keys='Tlahtolli.user_id', lazy='dynamic')
+    authored = db.relationship('Lesson', foreign_keys='Lesson.author_id', lazy='dynamic')
     registered = db.Column(db.DateTime, default=datetime.utcnow)
     ###Add a user app interaction/activity db
     #TODO a sparsematrix of the lesson id and the thumb up or down
     #positive and negative ratings (1, 0, or -1)
-    reviews = db.relationship('Lesson', backref='liked_by', lazy='dynamic') #add thumbs_down
+    reviews = db.relationship('Lesson', foreign_keys='Lesson.liked_by', lazy='dynamic') #add thumbs_down
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # viewed = db.relationship('Lesson', backref='', lazy='dynamic') This may be necessary come time in the future, but not today
 
@@ -156,7 +156,7 @@ class Lesson(PaginatedAPIMixin, db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     liked_by = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    tags = db.Column(db.LargeBinary(120))
+    tags = db.Column(db.LargeBinary(120), default=b'\x00'*120)
     hash_id = db.Column(db.String(120))
     prev = db.Column(db.Integer)
     next = db.Column(db.Integer)
