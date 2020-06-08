256544c831182df332c8b2424bb0e36f9380b278
xochozomatli <aavillaverde11@gmail.com>
2020-06-08 06:09:33 -0600

    fixes issue with review table

--- api/totlahtol.py
+++ api/totlahtol.py
@@ -1,8 +1,8 @@
 from app import create_app, db
-from app.models import User, Lesson, Tlahtolli
+from app.models import User, Lesson, Tlahtolli, Review
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db':db, 'User':User, 'Lesson':Lesson, 'Tlahtolli':Tlahtolli}
+    return {'db':db, 'User':User, 'Lesson':Lesson, 'Tlahtolli':Tlahtolli, 'Review':Review}
