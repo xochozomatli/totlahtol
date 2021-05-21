c47f6ddc1be1c0c488d47a252982aaed281a5911
xochozomatli <aavillaverde11@gmail.com>
2021-05-21 06:43:49 -0600

    delete old test folder; might put it back later, idk

--- api/tests/unit/test_client.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-def test_example(client):
-    response = client.get("/api/lessons")
-    assert response.status_code == 200
--- api/tests/unit/test_models.py
+++
@@ -1,8 +0,0 @@
-from app.models  import User
-
-def test_simplest():
-    assert True
-
-def test_user():
-    u = User()
-    assert User() is not None
